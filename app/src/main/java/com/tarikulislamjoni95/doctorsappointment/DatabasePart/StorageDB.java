e2f85d9c90b267c82fd75d16d09897069c473a3e
tarikul-islam-joni <tarikulislamjoni95@gmail.com>
2019-08-08 15:48:20 +0600

    Project Reformation

--- app/src/main/java/com/tarikulislamjoni95/doctorsappointment/DatabasePart/StorageDB.java
+++ app/src/main/java/com/tarikulislamjoni95/doctorsappointment/DatabasePart/StorageDB.java
@@ -68,6 +68,7 @@ public class StorageDB
                     {
                         progressDialog.dismiss();
                     }
+                    //
                 }
             });
         }
